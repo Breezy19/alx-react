d1e78cb0ea0d103808afcfea43ee419a4bc0663b
“Breezy19” <freij.akram@gmail.com>
2024-02-03 16:11:05 -0800

    Webpack

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -2,33 +2,37 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: path.resolve(__dirname, './js/dashboard_main.js'),
+  entry: './js/dashboard_main.js',
   performance: {
     maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
-          "file-loader",
+          'file-loader',
           {
-            loader: "image-webpack-loader",
+            loader: 'image-webpack-loader',
             options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
+              bypassOnDebug: true,
+              disable: true,
             },
           },
         ],
       },
-    ],
+    ]
   },
 };
