39a437a7dd3bad7eeddf91c9811be2aa84e5ce42
“Breezy19” <freij.akram@gmail.com>
2024-02-03 16:22:27 -0800

    webpack

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -3,47 +3,36 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: './js/dashboard_main.js',
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/images/[hash][ext][query]'
-        }
-      }
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
     ]
   },
-  performance: {
-    hints: false
-  },
-  optimization: {
-    imageWebpackLoader: {
-      mozjpeg: {
-        progressive: true,
-        quality: 65
-      },
-      optipng: {
-        enabled: false,
-      },
-      pngquant: {
-        quality: [0.65, 0.90],
-        speed: 4
-      },
-      gifsicle: {
-        interlaced: false,
-      },
-      webp: {
-        quality: 75
-      }
-    }
-  }
 };
